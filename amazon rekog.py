84ff5f98d081b41f2c705e196fd5e10cdef1ff99
sampada101 <55329464+sampada101@users.noreply.github.com>
2020-10-02 18:53:43 +0545

    Add files via upload

---
+++ amazon rekog.py
@@ -0,0 +1,23 @@
+import boto3
+
+photo = 'pic.png'
+image_bucket = 'face-rekognition-kolagi'
+
+rekognition_client = boto3.client('rekognition', aws_access_key_id=aws_access_key_id,
+                                  aws_secret_access_key=aws_secret_access_key,
+                                  aws_session_token=aws_session_token,
+                                  region_name='us-east-1')
+
+detect_facial_attributes = rekognition_client.detect_faces(
+    Image={
+        'S3Object': {
+            'Bucket': image_bucket,
+            'Name': photo
+        }
+    },
+    Attributes=[
+        'ALL'
+    ]
+)
+
+print(detect_facial_attributes)
